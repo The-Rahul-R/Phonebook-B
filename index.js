e89f1ca5c3ea9ca6b6dd97dd8186bd1ec66c7496
Rahul R <rahulreddy23feb@gmail.com>
2024-06-21 17:33:24 +0530

    added frontend

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 morgan.token('post-data', function(req){ return req.method=== 'POST' ? JSON.stringify(req.body): ''})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'))
 
